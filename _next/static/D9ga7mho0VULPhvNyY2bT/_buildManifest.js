cdce1cfb2f7f7db97111259be7fe87dc7ed7d0e1
onumick <onumick@users.noreply.github.com>
2026-03-28 20:33:24 +0000

    deploy: 022a3a283615071cab2899a8f902b79595fbbeb0

---
+++ _next/static/D9ga7mho0VULPhvNyY2bT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ad7f724d-e6f475bd3df84b7b.js","static/chunks/392-0dd6c4b5a4b9c80b.js","static/chunks/pages/index-5e51526d92c7020a.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
