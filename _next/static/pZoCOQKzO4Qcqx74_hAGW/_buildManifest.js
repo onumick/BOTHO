81cbba662b4d4d0e3aba1b7f323947d2d5c494de
onumick <onumick@users.noreply.github.com>
2026-03-29 13:43:34 +0000

    deploy: 81a5ecad3176d53a5f7985b6f06368053db87fc2

--- _next/static/pZoCOQKzO4Qcqx74_hAGW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ad7f724d-e6f475bd3df84b7b.js","static/chunks/872-cb93f57ed23a6576.js","static/chunks/pages/index-ead3a8083c087605.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/zRBj3FuP_a2K7gO_1DdO3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ad7f724d-e6f475bd3df84b7b.js","static/chunks/872-cb93f57ed23a6576.js","static/chunks/pages/index-64d81d65553fbc30.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
